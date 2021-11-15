1b01bd3660d7c382ec98d6817c3afc017073eb40
GritWord <110587667+GritWord@users.noreply.github.com>
2021-11-15 09:43:52 +0000

    update ar translation

--- client/src/App.vue
+++ client/src/App.vue
@@ -55,4 +55,6 @@ export default {
     }
   },
   mounted () {
-    const burger = document.que
+    const burger = document.querySelector('.burger')
+    const mainnav = document.getElementById('burgernav')
+    burger.addEventListener('click', (
