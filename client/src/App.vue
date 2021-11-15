4641890b036d125ed216e68d4d0e6dcb960e1aba
GritWord <110587667+GritWord@users.noreply.github.com>
2021-11-15 18:25:58 +0000

    Add untracked files

--- client/src/App.vue
+++ client/src/App.vue
@@ -57,4 +57,8 @@ export default {
   mounted () {
     const burger = document.querySelector('.burger')
     const mainnav = document.getElementById('burgernav')
-    burger.addEventListener('click', (
+    burger.addEventListener('click', () => {
+      mainnav.classList.toggle('active')
+      burger.classList.toggle('active')
+    })
+    this.t
