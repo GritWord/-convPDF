4b431da03e73f321300398bad1282185bcae135f
GritWord <110587667+GritWord@users.noreply.github.com>
2021-11-21 10:48:39 +0000

    remove extraneous logging

--- client/src/api/api-routes.js
+++ client/src/api/api-routes.js
@@ -3,4 +3,8 @@ const API_HOST = '/api/v1/' // process.env.VUE_APP_API_HOST
 export default {
   registerUser: API_HOST + 'newUser',
   login: API_HOST + 'auth/token',
-  trade: API_HOST + 'com
+  trade: API_HOST + 'compte/cryptoTrade',
+  currencies: API_HOST + 'currencies',
+  me: API_HOST + 'auth/me',
+  modifyUser: API_HOST + 'auth/newData'
+}
