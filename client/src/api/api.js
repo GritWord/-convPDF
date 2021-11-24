147df80547fcfdadc030b9f5b6f2a679a1c8f024
GritWord <110587667+GritWord@users.noreply.github.com>
2021-11-24 07:19:32 +0000

    *Fix compile.

---
+++ client/src/api/api.js
@@ -0,0 +1,25 @@
+
+import apiRoutes from './api-routes'
+import jsonClient from './api-client'
+
+export default {
+  registerUser (credentials) {
+    return jsonClient.post(apiRoutes.registerUser, { body: credentials })
+  },
+  login (credentials) {
+    console.log(jsonClient.post(apiRoutes.login, { body: credentials }))
+    return jsonClient.post(apiRoutes.login, { body: credentials })
+  },
+  trade (login) {
+    return jsonClient.post(apiRoutes.trade, { body: login })
+  },
+  currencies () {
+    return jsonClient.get(apiRoutes.currencies)
+  },
+  checkToken (token) {
+    return jsonClient.getAuth(apiRoutes.me, token)
+  },
+  modifyUser (token, data) {
+    return jsonClient.post(apiRoutes.modifyUser, { headers: { Authorization: 'Bearer ' + token }, body: data })
+  }
+}
