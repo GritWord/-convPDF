afbf9d35f975b93d5eb8d7ff6a9c104615b568a2
GritWord <110587667+GritWord@users.noreply.github.com>
2021-12-02 06:55:08 +0000

    Minor change.

--- client/src/main.js
+++ client/src/main.js
@@ -5,4 +5,4 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use
+createApp(App).use(store).use(router).component('fa-icon', FontAwesomeIcon).mount('#app')
