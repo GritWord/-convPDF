9ffe3829539fc0f925c8d4feda5311b5ec282195
GritWord <110587667+GritWord@users.noreply.github.com>
2021-12-02 11:06:28 +0000

    23

--- client/src/plugins/font-awesome.js
+++ client/src/plugins/font-awesome.js
@@ -1,1 +1,3 @@
-import { library } from '@fortawesome/fontawesome-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-font
