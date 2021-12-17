c760d628d06991eb4776811d12dee8a333660a6a
GritWord <110587667+GritWord@users.noreply.github.com>
2021-12-17 12:15:56 +0000

    Update DD.jl

--- client/src/views/Comprendre.vue
+++ client/src/views/Comprendre.vue
@@ -1,3 +1,8 @@
 <template>
   <div class="cours">
-    <router-link class="cours_gauche" to="/cours/b
+    <router-link class="cours_gauche" to="/cours/blockchain">
+      <img
+        class="imgBlockchain"
+        src="../images/blockchain_icon.png"
+        alt="image Blockchain"
+    
