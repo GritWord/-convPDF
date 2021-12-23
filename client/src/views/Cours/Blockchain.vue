6a52871099f81d5a2385cc52a50ddae2355bb98e
GritWord <110587667+GritWord@users.noreply.github.com>
2021-12-23 19:49:34 +0000

    Started README

--- client/src/views/Cours/Blockchain.vue
+++ client/src/views/Cours/Blockchain.vue
@@ -1,3 +1,5 @@
 <template>
   <h1 class="cours-titre">Qu’est-ce que la blockchain ?</h1>
-  <h3 
+  <h3 class="cours-soustitre">Définition et explication</h3>
+  <p class="cours-paragraphe">
+    La blockcha
