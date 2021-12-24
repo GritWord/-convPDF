7b045172a9c78e569da35dcdb158c9cbbf8a9d9b
GritWord <110587667+GritWord@users.noreply.github.com>
2021-12-24 17:19:35 +0000

    update users

--- client/src/views/Cours/Blockchain.vue
+++ client/src/views/Cours/Blockchain.vue
@@ -2,4 +2,5 @@
   <h1 class="cours-titre">Qu’est-ce que la blockchain ?</h1>
   <h3 class="cours-soustitre">Définition et explication</h3>
   <p class="cours-paragraphe">
-    La blockcha
+    La blockchain est une technologie de stockage et de transmission
+    d’informations, transparente, sécurisée, et fonct
