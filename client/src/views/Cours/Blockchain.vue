b666af6cbecff3181ef54890ab189abba13ab21d
GritWord <110587667+GritWord@users.noreply.github.com>
2021-12-24 22:15:08 +0000

    svn propset svn:eol-style native

--- client/src/views/Cours/Blockchain.vue
+++ client/src/views/Cours/Blockchain.vue
@@ -3,4 +3,6 @@
   <h3 class="cours-soustitre">Définition et explication</h3>
   <p class="cours-paragraphe">
     La blockchain est une technologie de stockage et de transmission
-    d’informations, transparente, sécurisée, et fonct
+    d’informations, transparente, sécurisée, et fonctionnant sans organe
+    central de contrôle (définition de Blockchain France). Elle est la
+    technologie au coeur du Web Dece
