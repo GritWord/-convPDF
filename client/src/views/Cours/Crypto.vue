721c6c5df90f82d4db8f72e2868f57488daf09c2
GritWord <110587667+GritWord@users.noreply.github.com>
2022-04-10 21:15:32 +0000

    Fix a bug

--- client/src/views/Cours/Crypto.vue
+++ client/src/views/Cours/Crypto.vue
@@ -2,4 +2,6 @@
   <h1 class="cours-titre">Qu’est-ce qu’une crypto monnaie ?</h1>
   <p class="cours-paragraphe">
     On désigne par crypto monnaie à la fois une monnaie cryptographique et
-    un système de paiement de pair à p
+    un système de paiement de pair à pair. Ces monnaies numériques sont
+    donc des monnaies virtuelles dans le sens où ces dernières sont
+    caractérisées 
