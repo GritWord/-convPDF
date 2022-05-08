b4d98e46b8fa80f384d82ef66b52ea30bb441e32
GritWord <110587667+GritWord@users.noreply.github.com>
2022-05-08 20:30:31 +0000

    udf: Fix data corruption for files in ICB

--- client/src/views/Graphiques.vue
+++ client/src/views/Graphiques.vue
@@ -4,4 +4,8 @@
       <li class="info-currency">
         <p class="currency-name" style="color: #001184">Nom</p>
         <p class="currency-price" style="color: #001184">Prix</p>
-        <p class="pr
+        <p class="priceIncrease" style="color: #001184">Cours 24h</p>
+      </li>
+      <li
+        class="info-currency"
+    
