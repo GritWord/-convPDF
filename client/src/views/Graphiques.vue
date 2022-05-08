5a9bf52e8e4144f96d9c046a6c4e0b7eeddac091
GritWord <110587667+GritWord@users.noreply.github.com>
2022-05-08 10:46:19 +0000

    pub update

--- client/src/views/Graphiques.vue
+++ client/src/views/Graphiques.vue
@@ -2,4 +2,6 @@
   <div>
     <ul>
       <li class="info-currency">
-        <p class="currency-name" style="co
+        <p class="currency-name" style="color: #001184">Nom</p>
+        <p class="currency-price" style="color: #001184">Prix</p>
+        <p class="pr
