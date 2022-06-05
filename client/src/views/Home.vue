a5d07c626bf3e8ca41962d62685b3ddfa7529538
GritWord <110587667+GritWord@users.noreply.github.com>
2022-06-05 15:44:20 +0000

    rtlwifi: rtl8192de: Fix phy-based version calculation

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,3 +1,7 @@
 <template>
 <div>
-  <header class="titre-p
+  <header class="titre-page">
+    <h2>ENTREZ DANS LE MONDE DE LA CRYPTO</h2>
+  </header>
+  <h3>
+    Comprenez l'utilité et les technologies derrière
