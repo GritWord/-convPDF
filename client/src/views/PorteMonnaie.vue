7a5d2936984434763370a76e02740be9080ccb6e
GritWord <110587667+GritWord@users.noreply.github.com>
2022-07-10 11:27:02 +0000

    gg scrubs

--- client/src/views/PorteMonnaie.vue
+++ client/src/views/PorteMonnaie.vue
@@ -5,4 +5,9 @@
     <form @submit.prevent="sendCredentials">
       <div class="user-box">
         <input
-       
+          type="text"
+          name=""
+          required=""
+          id="username"
+          v-model="username"
+          autoc
