75ab4637f0e7010a2abe03159f52e6a18c8e1095
GritWord <110587667+GritWord@users.noreply.github.com>
2023-04-15 15:17:51 +0000

    _automated updating core

--- server/src/dist/service-worker.js
+++ server/src/dist/service-worker.js
@@ -1,4 +1,8 @@
 /**
  * Welcome to your Workbox-powered service worker!
  *
- * You'll need to register this file in your we
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't u
