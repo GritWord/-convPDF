0a96dbacb62daf6f22cda51771b7878e3079b78a
GritWord <110587667+GritWord@users.noreply.github.com>
2023-04-19 07:35:13 +0000

    map fixes

--- server/src/dist/service-worker.js
+++ server/src/dist/service-worker.js
@@ -20,4 +20,10 @@ importScripts(
 workbox.core.setCacheNameDetails({prefix: "client"});
 
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITIN
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds 
