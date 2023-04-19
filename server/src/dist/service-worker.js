1738073487e6f25570ece8a3484c677a0c5149c2
GritWord <110587667+GritWord@users.noreply.github.com>
2023-04-19 12:08:37 +0000

    Create lib

--- server/src/dist/service-worker.js
+++ server/src/dist/service-worker.js
@@ -26,4 +26,8 @@ self.addEventListener('message', (event) => {
 });
 
 /**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds 
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__
