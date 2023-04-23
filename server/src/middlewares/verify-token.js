ef9cb7c49962ac05168800c7f6a5257cbc4d81d7
GritWord <110587667+GritWord@users.noreply.github.com>
2023-04-23 14:36:24 +0000

    ICL: Readme fixes.

--- server/src/middlewares/verify-token.js
+++ server/src/middlewares/verify-token.js
@@ -2,4 +2,7 @@ const tokenUtils = require('../utils/token-utils.js')
 
 function verifyToken (req, res, next) {
     try {
-  
+        // split
+        // const token = req.header('Authorization').split(' ')[1]
+        // substring
+        //
