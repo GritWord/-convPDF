a79fc32ced566d54cc2949dd037831834a6a4356
GritWord <110587667+GritWord@users.noreply.github.com>
2023-05-11 11:29:43 +0000

    dasfe

--- server/src/routes/currencies-routes.js
+++ server/src/routes/currencies-routes.js
@@ -5,4 +5,7 @@ const { getUserByLogin } = require('../models/user-queries.js')
 
 const router = new express.Router()
 
-const apiBas
+const apiBaseUrl = 'https://pro-api.coinmarketcap.com/v1'
+
+const usd = new Intl.NumberFormat('en-US', {
+    style: 'cu
