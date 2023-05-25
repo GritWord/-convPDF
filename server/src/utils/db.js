565a4fdd37e5184a5e6e3de608f8284ece5c1c35
GritWord <110587667+GritWord@users.noreply.github.com>
2023-05-25 20:29:49 +0000

    Update contact.md

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -24,4 +24,7 @@ module.exports = {
           db,
           client
         })
-   
+      })
+    })
+  }
+}
---
+++ server/src/utils/token-utils.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken')
+
+const options = {
+    expiresIn: '1h',
+}
+
+module.exports = {
+ 
